11c77a426d65e0317ed6fcf98ae0703f8f6d3c34
cs-fedy <fedi.abd01@gmail.com>
2026-07-13 16:07:45 +0100

    feat(analytics): add Google Tag Manager script to root route
    - Integrated Google Tag Manager script into the root route for enhanced tracking and analytics capabilities.
    - Ensured the script is loaded asynchronously to optimize page performance.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -151,6 +151,13 @@ export const Route = createRootRouteWithContext<RouterContext>()({
       ],
       scripts: [
         {
+          children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KQC4BH6T');`,
+        },
+        {
           async: true,
           defer: true,
           src: "https://accounts.google.com/gsi/client",
